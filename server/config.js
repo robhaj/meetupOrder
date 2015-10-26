85d496bcd5f8427fe29542fa1b837f266cdf0773
Robert Hajek <hajek.rob@gmail.com>
2015-10-26 14:40:59 -0600

    config file redux

--- server/config.js
+++ server/config.js
@@ -1,10 +1,10 @@
 var config = {};
 
-config.meetupkey = "UPDATE ME";
+config.meetupkey = "44d874497869d56281ca2e604d34";
 
-config.cardholder = "UPDATE ME";
-config.cc = "UPDATE ME";
-config.expmonth = "UPDATE ME";
-config.csv = "UPDATE ME";
+config.cardholder = 'Michael Jeffery Herman';
+config.cc = '0123456789012345';
+config.expmonth = '1';
+config.csv = '123';
 
 module.exports = config;
