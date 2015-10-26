71d8d3b6728cda34eef317d2468db0136e360fd3
Robert Hajek <hajek.rob@gmail.com>
2015-10-26 14:38:37 -0600

    fixing config file

--- server/config.js
+++ server/config.js
@@ -1,10 +1,10 @@
 var config = {};
 
-config.meetupkey = "44d874497869d56281ca2e604d34";
+config.meetupkey = "UPDATE ME";
 
-config.cardholder = 'Michael Jeffery Herman';
-config.cc = '0123456789012345';
-config.expmonth = '1';
-config.csv = '123';
+config.cardholder = "UPDATE ME";
+config.cc = "UPDATE ME";
+config.expmonth = "UPDATE ME";
+config.csv = "UPDATE ME";
 
 module.exports = config;
