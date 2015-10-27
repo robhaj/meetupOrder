d5e49396a814bf9085d43bf809802f92457d33bd
Michael Herman <hermanmu@gmail.com>
2015-10-27 14:20:59 -0600

    client side updates

--- client/app.js
+++ client/app.js
@@ -3,7 +3,7 @@ var myApp = angular.module("myApp", ["ngRoute"]);
 myApp.config(function($routeProvider) {
   $routeProvider
   .when("/", {
-    templateUrl: "partails/home.html",
+    templateUrl: "partials/home.html",
     controller: "PizzaController"
   })
   .otherwise({
