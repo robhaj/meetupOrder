90b292a4911964bf6016a856fc20249f4f36e92f
Robert Hajek <hajek.rob@gmail.com>
2015-10-29 15:34:49 -0600

    added addtl pizza quantites dynamically

--- server/webdriver.js
+++ server/webdriver.js
@@ -10,6 +10,7 @@ function initWD(meetupInfo) {
   var veggieQuan = meetupInfo.quantities[2];
   var quantity = meetupInfo.quantities[3];
 
+  //instatiate webdriver
   var driver = new webdriver.Builder()
   .forBrowser('chrome')
   .build();
@@ -19,7 +20,7 @@ function initWD(meetupInfo) {
   driver.findElement(By.id('UserName')).sendKeys(meetupInfo.user_email);
   driver.findElement(By.id('Password')).sendKeys(meetupInfo.user_password);
   driver.findElement(By.className('ui-button')).click();
-  driver.sleep(3000);
+  driver.sleep(2000);
 
   //order type and address
   driver.findElement(By.className('delivery')).click();
@@ -47,12 +48,11 @@ function initWD(meetupInfo) {
       driver.findElement(By.name('i1_mod_m33')).click();
       driver.sleep(3000);
 
-      //selects half pepperoni
-      var z = driver.findElements(By.className('mods-h1-active'));
-      z.then(function(pepperoni){
-        pepperoni[39].click();
-        driver.findElement(By.id('i1_qty')).clear();
-        driver.findElement(By.id('i1_qty')).sendKeys(quantity);
+      //send quantity
+      var q = driver.findElement(By.id('i1_qty'));
+      q.then(function(quantity){
+        quantity.clear();
+        quantity.sendKeys(pepQuan);
         driver.sleep(3000);
 
         //adds to cart
@@ -61,14 +61,73 @@ function initWD(meetupInfo) {
           add.click();
           driver.sleep(3000);
 
-          //checkout
-          driver.findElement(By.id('lnkCheckout')).click();
-          driver.sleep(2000);
-          driver.findElement(By.id('CreditCard')).sendKeys(config.cc);
-          driver.findElement(By.id('CardHolderName')).sendKeys(config.cardholder);
-          driver.findElement(By.id('ExpMonth')).sendKeys(config.expmonth);
-          driver.findElement(By.id('ExpYear')).sendKeys(config.expyear);
-          driver.findElement(By.id('SecurityCode')).sendKeys(config.csv);
+          //selects cheese
+          var x = driver.findElement(webdriver.By.xpath('//*[@id="Pizza"]/div/div/div[1]/div/button/span[2]'));
+          x.then(function(cheese){
+            cheese.click();
+            driver.sleep(5000);
+
+            //selects xlarge
+            var y = driver.findElements(By.className('SzPrice'));
+            y.then(function(xlarge){
+              xlarge[4].click();
+              driver.sleep(3000);
+
+              //send quantity
+              var q = driver.findElement(By.id('i1_qty'));
+              q.then(function(quantity){
+                quantity.clear();
+                quantity.sendKeys(cheeseQuan);
+                driver.sleep(3000);
+
+                //adds to cart
+                var k = driver.findElement(By.xpath('/html/body/div[4]/div[3]/div/button/span'));
+                k.then(function(add){
+                  add.click();
+                  driver.sleep(3000);
+
+                  //selects veggie
+                  var v = driver.findElement(By.xpath('//*[@id="Pizza"]/div/div/div[4]/div/button/span[2]'));
+                  v.then(function(veggie){
+                    veggie.click();
+                    driver.sleep(3000);
+
+                    //selects xlarge
+                    var y = driver.findElements(By.className('SzPrice'));
+                    y.then(function(xlarge){
+                      xlarge[4].click();
+                      driver.sleep(3000);
+
+                      //send quantity
+                      var q = driver.findElement(By.id('i4_qty'));
+                      q.then(function(quantity){
+                        quantity.clear();
+                        quantity.sendKeys(veggieQuan);
+                        driver.sleep(3000);
+
+                        //adds to cart
+                        var k = driver.findElement(By.xpath('/html/body/div[4]/div[3]/div/button/span'));
+                        k.then(function(add){
+                          add.click();
+                          driver.sleep(3000);
+
+                          //checkout
+                          driver.findElement(By.id('lnkCheckout')).click();
+                          driver.sleep(2000);
+                          driver.findElement(By.id('CreditCard')).sendKeys(config.cc);
+                          driver.findElement(By.id('CardHolderName')).sendKeys(config.cardholder);
+                          driver.findElement(By.id('ExpMonth')).sendKeys(config.expmonth);
+                          driver.findElement(By.id('ExpYear')).sendKeys(config.expyear);
+                          driver.findElement(By.id('SecurityCode')).sendKeys(config.csv);
+                          driver.quit();
+                        });
+                      });
+                    });
+                  });
+                });
+              });
+            });
+          });
         });
       });
     });
